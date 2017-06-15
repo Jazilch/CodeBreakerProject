96f4ce07fca9595234ab78a438e62d4cff3f3c9e
Jazilch <zilchjames5@gmail.com>
2017-06-15 08:39:39 -0400

    V3

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,7 +6,28 @@ function guess() {
     if(answer.value = ' ' || attempt.value = ' '){
       setHiddenFields();
     }
+
+    if(!validateInput(input.value)){
+      return;
+    }
+    attempt.value++;
 }
+
+
+function getResults(input){
+  let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+  for(i = 0; i < input.length; i++){
+    if(input.charAt(i) == answer.value.charAt(i)){
+      html += '<span class="glyphicon glyphicon-ok"></span>';
+    } else if(answer.value.indexOf(input.charAt(i)) > -1){
+      html += '<span class="glyphicon glyphicon-transfer"></span>';
+    }else{
+      html += '<span class="glyphicon glyphicon-remove"></span>';
+    }
+  }
+}
+html += '</div></div>';
+document.getElementById('results').innerHTML += html;
 
 function setHiddenFields(){
   answer.value = Math.floor(Math.random() * 10000).toString();
@@ -26,5 +47,4 @@ function validateInput(input){
     return false;
   }
     return true;
-
 }
